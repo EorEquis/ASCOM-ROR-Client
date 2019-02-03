395b913db2674264008f70dece7518ee41062b44
EorEquis <eeyore@stuffupthere.com>
2019-02-03 11:25:17 -0600

    Initial Commit

---
+++ RORClient/frmMain.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.labelDriverId = New System.Windows.Forms.Label()
+        Me.buttonConnect = New System.Windows.Forms.Button()
+        Me.buttonChoose = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnHalt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'labelDriverId
+        '
+        Me.labelDriverId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.labelDriverId.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.ASCOM.TriStar.My.MySettings.Default, "DriverId", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.labelDriverId.Location = New System.Drawing.Point(12, 9)
+        Me.labelDriverId.Name = "labelDriverId"
+        Me.labelDriverId.Size = New System.Drawing.Size(291, 21)
+        Me.labelDriverId.TabIndex = 5
+        Me.labelDriverId.Text = Global.ASCOM.TriStar.My.MySettings.Default.DriverId
+        Me.labelDriverId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'buttonConnect
+        '
+        Me.buttonConnect.Location = New System.Drawing.Point(316, 36)
+        Me.buttonConnect.Name = "buttonConnect"
+        Me.buttonConnect.Size = New System.Drawing.Size(72, 23)
+        Me.buttonConnect.TabIndex = 4
+        Me.buttonConnect.Text = "Connect"
+        Me.buttonConnect.UseVisualStyleBackColor = True
+        '
+        'buttonChoose
+        '
+        Me.buttonChoose.Location = New System.Drawing.Point(316, 7)
+        Me.buttonChoose.Name = "buttonChoose"
+        Me.buttonChoose.Size = New System.Drawing.Size(72, 23)
+        Me.buttonChoose.TabIndex = 3
+        Me.buttonChoose.Text = "Choose"
+        Me.buttonChoose.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(316, 65)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(72, 23)
+        Me.btnOpen.TabIndex = 6
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(316, 94)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(72, 23)
+        Me.btnClose.TabIndex = 7
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Dome Driver"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Status"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblStatus.Location = New System.Drawing.Point(12, 67)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(291, 21)
+        Me.lblStatus.TabIndex = 9
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'btnHalt
+        '
+        Me.btnHalt.BackColor = System.Drawing.Color.Red
+        Me.btnHalt.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnHalt.Location = New System.Drawing.Point(316, 123)
+        Me.btnHalt.Name = "btnHalt"
+        Me.btnHalt.Size = New System.Drawing.Size(72, 23)
+        Me.btnHalt.TabIndex = 11
+        Me.btnHalt.Text = "HALT"
+        Me.btnHalt.UseVisualStyleBackColor = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 154)
+        Me.Controls.Add(Me.btnHalt)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.labelDriverId)
+        Me.Controls.Add(Me.buttonConnect)
+        Me.Controls.Add(Me.buttonChoose)
+        Me.Name = "frmMain"
+        Me.Text = "Open/Close Roof"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents labelDriverId As System.Windows.Forms.Label
+    Private WithEvents buttonConnect As System.Windows.Forms.Button
+    Private WithEvents buttonChoose As System.Windows.Forms.Button
+    Friend WithEvents btnOpen As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Private WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents btnHalt As System.Windows.Forms.Button
+
+End Class
---
+++ RORClient/frmMain.vb
@@ -0,0 +1,102 @@
+﻿Public Class frmMain
+
+    Dim ShutterState() As String = {"Open", "Closed", "Opening", "Closing", "Error"}
+    Private driver As ASCOM.DriverAccess.Dome
+
+    ''' <summary>
+    ''' This event is where the driver is choosen. The device ID will be saved in the settings.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="System.EventArgs" /> instance containing the event data.</param>
+    Private Sub buttonChoose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonChoose.Click
+        My.Settings.DriverId = ASCOM.DriverAccess.Dome.Choose(My.Settings.DriverId)
+        SetUIState()
+    End Sub
+
+    ''' <summary>
+    ''' Connects to the device to be tested.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="System.EventArgs" /> instance containing the event data.</param>
+    Private Sub buttonConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonConnect.Click
+        If (IsConnected) Then
+            Timer1.Enabled = False
+            driver.Connected = False
+        Else
+            driver = New ASCOM.DriverAccess.Dome(My.Settings.DriverId)
+            driver.Connected = True
+            Timer1.Enabled = True
+        End If
+        SetUIState()
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If IsConnected Then
+            driver.Connected = False
+        End If
+        ' the settings are saved automatically when this application is closed.
+    End Sub
+
+    ''' <summary>
+    ''' Sets the state of the UI depending on the device state
+    ''' </summary>
+    Private Sub SetUIState()
+        buttonConnect.Enabled = Not String.IsNullOrEmpty(My.Settings.DriverId)
+        buttonChoose.Enabled = Not IsConnected
+        btnClose.Enabled = IsConnected
+        btnOpen.Enabled = IsConnected
+        buttonConnect.Text = IIf(IsConnected, "Disconnect", "Connect")
+        If IsConnected Then ' Why the hell IIf doesn't work here, I don't know...but it doesn't.  It breaks.
+            lblStatus.Text = ShutterState(driver.ShutterStatus)
+        Else
+            lblStatus.Text = ""
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Gets a value indicating whether this instance is connected.
+    ''' </summary>
+    ''' <value>
+    ''' 
+    ''' <c>true</c> if this instance is connected; otherwise, <c>false</c>.
+    ''' 
+    ''' </value>
+    Private ReadOnly Property IsConnected() As Boolean
+        Get
+            If Me.driver Is Nothing Then Return False
+            Return driver.Connected
+        End Get
+    End Property
+
+    ' TODO: Add additional UI and controls to test more of the driver being tested.
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblStatus.Text = ShutterState(driver.ShutterStatus)
+    End Sub
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        If IsConnected Then
+            driver.OpenShutter()
+            btnOpen.Enabled = False
+            btnClose.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        If IsConnected Then
+            driver.CloseShutter()
+            btnOpen.Enabled = True
+            btnClose.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnHalt_Click(sender As Object, e As EventArgs) Handles btnHalt.Click
+        If IsConnected Then
+            driver.AbortSlew()
+            btnOpen.Enabled = True
+            btnClose.Enabled = True
+        End If
+    End Sub
+End Class
